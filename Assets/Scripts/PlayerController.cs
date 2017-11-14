b5e1a99d65ef31e2cf96c6f765615f692eb30307
Watson Li <watsonli@Watsons-MacBook-Pro.local>
2017-11-13 19:06:17 -0500

    rollaball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private int count;
+	public Text countText;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		setCountText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText (){
+		countText.text = "Count: " + count;
+	}
+}
